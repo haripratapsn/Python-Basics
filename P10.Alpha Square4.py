e74ad883162c614cef4d50efb56673aab2ca4c0d
Hari Pratap SN <haripratapsn2003@gmail.com>
2025-09-01 00:18:29 +0530

    Code Added

---
+++ P10.Alpha Square4.py
@@ -0,0 +1,13 @@
+# abcd
+# abcd
+# abcd
+# abcd
+
+
+sides=int(input("Enter the length of the Square: "))
+
+for i in range(1,sides+1):
+    for j in range(1,sides+1):
+        print(chr(96+j), end=' ')
+    print()
+
