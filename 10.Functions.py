5c05858b2f2e7395365ed8f144dcc975590973f5
Hari Pratap SN <haripratapsn2003@gmail.com>
2026-02-16 07:50:07 +0530

    Modified Functions and Added Decorators

--- 10.Functions.py
+++ 10.Functions.py
@@ -1,5 +1,15 @@
+#functions Without arguments
+
 def fun():
     print("fun calling")
 print("Before fun calling")
 fun()
 print("After Fun calling")
+
+#function With arguments
+
+def Date(d,m,y):
+    print(d,m,y,sep="-")
+
+print(Date("21","09","2003"))
+
