c4c9a5ceff9ce84b2e8460f2164780871877b220
Hari Pratap SN <haripratapsn2003@gmail.com>
2026-02-27 15:55:54 +0530

    Abstraction code added

--- 15.Abstraction.py
+++ 15.Abstraction.py
@@ -9,3 +9,24 @@ class Shape(ABC):
     @abstractmethod
     def getArea(length,width):
         pass
+
+class Person:
+    def __init__(self, name=None, age=None):
+        self.__name = name
+        self.__age = age
+
+    def get_name(self):
+        if self.__name is None:
+            return "None"
+        return self.__name
+
+    def get_age(self):
+        if self.__age is None:
+            return "None"
+        return self.__age
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_age(self, age):
+        self.__age = age
